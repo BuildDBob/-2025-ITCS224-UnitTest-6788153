e64fc72426ea577d87f590ee2a04580de07333f8
BuildDBob <90126152+BuildDBob@users.noreply.github.com>
2026-04-07 08:25:48 +0000

    add test cases

--- test_subtest_age.py
+++ test_subtest_age.py
@@ -3,12 +3,23 @@ from age import categorize_by_age
 
 class TestIsEven(unittest.TestCase): 
 
+    def test_is_child(self): 
+        for number in [0,1,2,3,4,5,6,7,8,9]: 
+            with self.subTest(number=number): 
+                self.assertEqual(categorize_by_age(number), "Child")
+                print(f"\n{number} is considered as a Child.")
+
     def test_is_adolescent(self): 
         for number in [10, 11, 12, 13, 14, 15, 16, 17, 18]: 
             with self.subTest(number=number): 
                 self.assertEqual(categorize_by_age(number), "Adolescent")
                 print(f"\n{number} is considered as an Adolescent.")
- 
+
+    def test_is_adult(self):
+        for number in range(19, 66): 
+            with self.subTest(number=number): 
+                self.assertEqual(categorize_by_age(number), "Adult")
+                print(f"\n{number} is considered as an Adult.")
 
 if __name__ == "__main__": 
 
